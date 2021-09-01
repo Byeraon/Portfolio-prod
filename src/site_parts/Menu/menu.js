614be416c162c6496e0bff1d870835f4319e74ff
Sorrowrest <nvas3470@gmail.com>
2021-09-01 10:56:33 +0300

    Fix coloring

--- src/site_parts/Menu/menu.js
+++ src/site_parts/Menu/menu.js
@@ -14,11 +14,9 @@ export let Menu = (props) => {
           <h1>&nbsp;KRASNOV ~</h1>
         </div>
         <h2 className={style.top_text}>
-          Frontend{" "}
-          <span style={{ backgroundColor: "white", color: "#1f2029" }}>
-            &nbsp;Developer{" "}
-          </span>
-          &nbsp;
+          Frontend 
+          <span className={style.text_next}>
+          &nbsp;Developer</span>
         </h2>
       </div>
       <Links setCard={props.setCard}></Links>
